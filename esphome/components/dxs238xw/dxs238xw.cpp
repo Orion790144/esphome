9cc550dc5ee6b892b94f1f42fe52cff00c24f314
Rodrigo <rodrigo.gonzar@gmail.com>
2025-09-23 11:23:42 -0300

    Update dxs238xw.cpp Fix ; error

--- esphome/components/dxs238xw/dxs238xw.cpp
+++ esphome/components/dxs238xw/dxs238xw.cpp
@@ -162,7 +162,7 @@ void Dxs238xwComponent::update() {
 }
 
 void Dxs238xwComponent::dump_config() {
-  LOG_UPDATE_INTERVAL(this)
+  LOG_UPDATE_INTERVAL(this);
   ESP_LOGCONFIG(TAG, "*** COMPONENT VERSION: %s ***", SM_STR_COMPONENT_VERSION);
 }
 
