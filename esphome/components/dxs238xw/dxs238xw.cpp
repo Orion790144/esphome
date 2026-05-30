6af1b5bca02b4f9050822bde8e94035e3246682a
Orion790144 <o79014407@gmail.com>
2026-05-30 16:27:25 -0300

    Update dxs238xw.cpp

--- esphome/components/dxs238xw/dxs238xw.cpp
+++ esphome/components/dxs238xw/dxs238xw.cpp
@@ -553,7 +553,7 @@ bool Dxs238xwComponent::receive_serial_data_(uint8_t *array, uint8_t type_messag
           ESP_LOGV(TAG, "* WRONG_BYTES: HEKR_LENGTH / Expected = %u, Receive = %u", size_expected, array[1]);
           read_error = SmErrorCode::WRONG_BYTES_LENGTH;
           break;
-        } else if (index_size == 2 && array[2] != type_message) {
+        } else if (index_size == 2 && array[2] != type_message && array[2] != 0xFE) {
           ESP_LOGV(TAG, "* WRONG_BYTES: HEKR_TYPE_MESSAGE / Expected = %u, Receive = %u", type_message, array[2]);
           read_error = SmErrorCode::WRONG_BYTES_TYPE_MESSAGE;
           break;
