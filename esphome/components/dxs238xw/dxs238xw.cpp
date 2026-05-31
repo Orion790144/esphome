6904494b4a8cc79b73acddfe809b59db32afbaf0
Orion790144 <o79014407@gmail.com>
2026-05-31 01:26:12 -0300

    Update dxs238xw.cpp

--- esphome/components/dxs238xw/dxs238xw.cpp
+++ esphome/components/dxs238xw/dxs238xw.cpp
@@ -622,13 +622,13 @@ bool Dxs238xwComponent::pre_receive_serial_data_(uint8_t cmd) {
 void Dxs238xwComponent::process_and_update_data_(const uint8_t *receive_array) {
   switch (receive_array[4]) {
     case HEKR_CMD_RECEIVE_METER_STATE: {
-                        this->ms_data_.time = millis();
+                              this->ms_data_.time = millis();
       this->ms_data_.phase_count = receive_array[5];
       this->ms_data_.meter_state = receive_array[6];
       this->ms_data_.delay_state = receive_array[18];
       this->ms_data_.delay_value_remaining = (receive_array[16] << 8) | receive_array[17];
 
-      // === LECTURAS QUE FUNCIONABAN ANTES ===
+      // === LECTURAS QUE FUNCIONABAN BIEN (firmware antiguo) ===
       UPDATE_SENSOR_MEASUREMENTS(total_energy, ((receive_array[8] << 16) | (receive_array[9] << 8) | receive_array[10]) * 0.01);
       UPDATE_SENSOR_MEASUREMENTS(active_power_phase_1, ((receive_array[13] << 8) | receive_array[14]) * 0.0001);
       UPDATE_SENSOR_MEASUREMENTS(voltage_phase_1, ((receive_array[8] << 8) | receive_array[9]) * 0.085);
