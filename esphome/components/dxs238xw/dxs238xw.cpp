9f6d92d8c827ff89d5ed8fce2eb644a0792b093f
Orion790144 <o79014407@gmail.com>
2026-05-31 00:51:08 -0300

    Update dxs238xw.cpp

--- esphome/components/dxs238xw/dxs238xw.cpp
+++ esphome/components/dxs238xw/dxs238xw.cpp
@@ -628,12 +628,12 @@ void Dxs238xwComponent::process_and_update_data_(const uint8_t *receive_array) {
       this->ms_data_.delay_state = receive_array[18];
       this->ms_data_.delay_value_remaining = (receive_array[16] << 8) | receive_array[17];
       
-            // Firmware antiguo - mediciones en mensaje de estado
+                  // Firmware antiguo - mediciones en mensaje de estado
       UPDATE_SENSOR_MEASUREMENTS(total_energy, ((receive_array[8] << 16) | (receive_array[9] << 8) | receive_array[10]) * 0.01);
       UPDATE_SENSOR_MEASUREMENTS(active_power_phase_1, ((receive_array[13] << 8) | receive_array[14]) * 0.0001);
 
-      // === CORRECCIÓN ACTUAL ===
-      UPDATE_SENSOR_MEASUREMENTS(voltage_phase_1, ((receive_array[8] << 8) | receive_array[9]) * 0.1);           // Voltaje (usando bytes 8-9)
+      // === NUEVOS FACTORES ===
+      UPDATE_SENSOR_MEASUREMENTS(voltage_phase_1, ((receive_array[8] << 8) | receive_array[9]) * 0.085);        // Voltaje
       UPDATE_SENSOR_MEASUREMENTS_CURRENT(current_phase_1, ((receive_array[13] << 8) | receive_array[14]) * 0.00045); // Corriente
       UPDATE_SENSOR_MEASUREMENTS(frequency, ((receive_array[15] << 8) | receive_array[16]) * 0.01);             // Frecuencia
       
