1c19406b479fe1fae63a474892a40bbd813a7f71
Orion790144 <o79014407@gmail.com>
2026-06-02 00:33:37 -0300

    Update dxs238xw.cpp

--- esphome/components/dxs238xw/dxs238xw.cpp
+++ esphome/components/dxs238xw/dxs238xw.cpp
@@ -539,8 +539,6 @@ void Dxs238xwComponent::process_and_update_data_(const uint8_t *receive_array) {
       this->ms_data_.delay_state = receive_array[18];
       this->ms_data_.delay_value_remaining = (receive_array[16] << 8) | receive_array[17];
 
-      UPDATE_SENSOR_MEASUREMENTS(active_power_phase_1, ((receive_array[13] << 8) | receive_array[14]) * 0.0001);
-
       if (this->ms_data_.meter_state) {
         this->ms_data_.warning_off_by_over_voltage = false;
         this->ms_data_.warning_off_by_under_voltage = false;
